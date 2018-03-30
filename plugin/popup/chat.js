47540481f44b1c48b4c07e2ee75362777775cc0e
Christina Liu <cyanliu@umich.edu>
2018-03-29 22:13:29 -0400

    cleaned up files

--- plugin/popup/chat.js
+++ plugin/popup/chat.js
@@ -1,37 +1,5 @@
-// class Space extends React.Component {
-//     constructor(props) {
-//         super(props);
-//     }
-
-//     render() {
-//         let s_but = null;
-//         let r_but = null;
-//         let content = null;
-//         return React.createElement(
-//             "div",
-//             null,
-//             "HelloWorld"
-//         );
-//     }
-// }
-
-// function App() {
-//     return React.createElement(
-//         "div",
-//         null,
-//         React.createElement(Space, null)
-//     );
-// }
-
-// ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
-
-
-// var socket = io.connect('http://localhost:3002');
-// socket.on('connect', function() {
-//   console.log('Client connected');
-// });
-
 $(function (){
+  // connect to socket.io
 	var socket = io.connect('http://localhost:3002');
 	$('form').submit(function(){
 		socket.emit('chat message', $('#m').val());
@@ -41,4 +9,4 @@ $(function (){
 	socket.on('chat message', function(msg){
 		$('#messages').append($('<li>').text(msg));
 	});
-});	
+});
