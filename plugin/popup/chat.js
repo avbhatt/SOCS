a6469fa3a80a2467d4cc80703b0e1b027a0e03d4
Adit Bhatt <avbhatt@umich.edu>
2018-03-29 21:59:53 -0400

    feat: initial working chat with no user or db saves

--- plugin/popup/chat.js
+++ plugin/popup/chat.js
@@ -1,26 +1,44 @@
-class Space extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+// class Space extends React.Component {
+//     constructor(props) {
+//         super(props);
+//     }
 
-    render() {
-        let s_but = null;
-        let r_but = null;
-        let content = null;
-        return React.createElement(
-            "div",
-            null,
-            "HelloWorld"
-        );
-    }
-}
+//     render() {
+//         let s_but = null;
+//         let r_but = null;
+//         let content = null;
+//         return React.createElement(
+//             "div",
+//             null,
+//             "HelloWorld"
+//         );
+//     }
+// }
 
-function App() {
-    return React.createElement(
-        "div",
-        null,
-        React.createElement(Space, null)
-    );
-}
+// function App() {
+//     return React.createElement(
+//         "div",
+//         null,
+//         React.createElement(Space, null)
+//     );
+// }
 
-ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
+// ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
+
+
+// var socket = io.connect('http://localhost:3002');
+// socket.on('connect', function() {
+//   console.log('Client connected');
+// });
+
+$(function (){
+	var socket = io.connect('http://localhost:3002');
+	$('form').submit(function(){
+		socket.emit('chat message', $('#m').val());
+		$('#m').val('');
+		return false;
+	});
+	socket.on('chat message', function(msg){
+		$('#messages').append($('<li>').text(msg));
+	});
+});	
