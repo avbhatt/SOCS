fae852e4423030083e05796eabec3bc8af1cff6c
Christina Liu <cyanliu@umich.edu>
2018-03-29 20:46:41 -0400

    a mini dropdown

--- plugin/popup/chat.js
+++ plugin/popup/chat.js
@@ -1,42 +1,40 @@
 class Space extends React.Component {
     constructor(props) {
         super(props);
-        this.handleUserTypeSelect = this.handleUserTypeSelect.bind(this); 
-    }
-
-    handleUserTypeSelect(event) {
-        console.log(event.value);
     }
 
     render() {
         let s_but = null;
         let r_but = null;
         let content = null;
-        const user_type_list = ["User", "Helper"];
-        const auth_obj = user_type_list.map(user_type => React.createElement(
-            "option",
-            { value: user_type, onChange: this.handleUserTypeSelect },
-            user_type
-        ));
-        console.log(user_type_list);
-        // return React.createElement(
-        //     "input",
-        //     null,
-        //     auth_obj
-        // );
-        return React.createElement(
-            "div",
-            null,
-            "Hello"
+        return React.createElement("select", null,
+         React.createElement("option", { value: 1 }, "1"),
+         React.createElement("option", { value: 2 }, "2")
         );
     }
 }
+
+// class Dropdown extends React.Component {
+//   constructor(props) {
+//       super(props);
+//   }
+//   render() {
+//     return (
+//       <option>
+//       <select>User</select>
+//       <select>Helper</select>
+//       </option>
+//     );
+//   }
+//
+// }
 
 function App() {
     return React.createElement(
         "div",
         null,
-        React.createElement(Space, null)
+        React.createElement(Space, null),
+        // React.createElement(Dropdown, null)
     );
 }
 
