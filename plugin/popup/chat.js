fc12f8591dea0f7c820d813f04c636027feec741
Ian McKenzie <ianphilm@umich.edu>
2018-03-29 20:31:29 -0400

    testing dropdown

--- plugin/popup/chat.js
+++ plugin/popup/chat.js
@@ -1,16 +1,33 @@
 class Space extends React.Component {
     constructor(props) {
         super(props);
+        this.handleUserTypeSelect = this.handleUserTypeSelect.bind(this); 
+    }
+
+    handleUserTypeSelect(event) {
+        console.log(event.value);
     }
 
     render() {
         let s_but = null;
         let r_but = null;
         let content = null;
+        const user_type_list = ["User", "Helper"];
+        const auth_obj = user_type_list.map(user_type => React.createElement(
+            "option",
+            { value: user_type, onChange: this.handleUserTypeSelect },
+            user_type
+        ));
+        console.log(user_type_list);
+        // return React.createElement(
+        //     "input",
+        //     null,
+        //     auth_obj
+        // );
         return React.createElement(
             "div",
             null,
-            "HelloWorld"
+            "Hello"
         );
     }
 }
