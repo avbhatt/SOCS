e64822bee3124d28421358593f48dae4ba88a27c
Adit Bhatt <avbhatt@umich.edu>
2018-04-02 22:57:50 -0400

    feat: helper and user chat -- need localStorage + db connects

--- plugin/sidebar/chat.js
+++ plugin/sidebar/chat.js
@@ -1,12 +1,37 @@
 $(function (){
-  // connect to socket.io
+	// connect to socket.io
 	var socket = io.connect('http://localhost:3002');
+
+	// connect to local storage to get type, default to user
+	var userType = getType();
+	var x = userType();
+
+	var recData;
+	// join personal room
+	socket.emit('join', {id: socket.id, website: $(location).attr('href'), type: x});
+
+	if (x){
+		socket.emit('first message', {id: socket.id, website: $(location).attr('href'), type: x, msg: "I'm in need of help!"});
+	}
+
 	$('form').submit(function(){
-		socket.emit('chat message', $('#m').val());
-		$('#m').val('');
+		if (recData.callbackID){
+			socket.emit('message', {id: socket.id, website: $(location).attr('href'), type: x, msg: $('#m').val(), callbackID: recData.callbackID})
+		}
+		else {
+			socket.emit('message', {id: socket.id, website: $(location).attr('href'), type: x, msg: $('#m').val()});
+			$('#m').val('');
+		}
 		return false;
 	});
-	socket.on('chat message', function(msg){
-		$('#messages').append($('<li>').text(msg));
+	socket.on('message', function(data){
+		$('#messages').append($('<li>').text(data.msg));
+		recData.callbackID = data.callbackID;
 	});
+
+	function getType(){
+		
+	}
+
 });
+
