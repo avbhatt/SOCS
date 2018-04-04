1dbfffc711ba33f0952663b0ab23957b5b1fe026
Ian McKenzie <ianphilm@umich.edu>
2018-04-04 17:19:29 -0400

    refactoring index2.js, adding functionality to deal with waiting users

--- index2.js
+++ index2.js
@@ -4,14 +4,15 @@ const MongoClient = require('mongodb').MongoClient
 const socketPort = 3002
 const serverPort = 3000
 var app = express();
-// Enable CORS
-app.use(cors());
+app.use(cors()); // Enable CORS
 
 var db;
 var socketServer;
 var io;
+var http_server;
 
-// server initializes connection to mongoDB
+// server code initializes by making connection to mongo 
+// and initializing socket and server info after connection
 MongoClient.connect('mongodb://server:wah123@ds127044.mlab.com:27044/wah_db', (err, client) => {
 	if (err) { return console.log(err); }
 
@@ -24,77 +25,18 @@ MongoClient.connect('mongodb://server:wah123@ds127044.mlab.com:27044/wah_db', (e
 	db.createCollection('message_logs', {});
 	// annotations schema: [{website: string, category: string, text: string}]
 	db.createCollection('annotations', {});
-
 	console.log("successful connection to Mongo DB");
-	console.log("opening connection to socketServer");
+
 	init_socket_server();
+	console.log("successful Socket.IO init");
+
+	init_http_server(); 
+	console.log("successful http server init"); 
 })
 
-// called when a new entity activates the browser extension by clicking a toolbar option ('User', 'Helper')
-function addEntity(id, type, website) {
-	// add entity to mongo active_entities collection
-	var new_entity = {
-		sock_id: id,
-		entity_type: type,
-		website: website,
-		is_chatting: false
-	}
-
-	db.collection('active_entities').save(new_entity, (err, result) => {
-			if (err) {
-			return console.log(err);
-			}
-			console.log("successful addEntity call"); 
-	});
-
-	// TODO: check for waiting users on the same webpage 
-}
-
-// called when an entity changes entity_type
-function updateEntityType(sock_id, type) {
-	// change an entity in the mongo collection--either website or type could change
-	db.collection('active_entities').updateOne({ sock_id: sock_id }, { $set: {entity_type: type}});
-
-	// TODO: check for waiting users on same webpage
-}
-
-// called when an entity changes website
-function updateEntityWebsite(sock_id, website) {
-	// change an entity in the mongo collection--either website or type could change
-	db.collection('active_entities').updateOne({ sock_id: sock_id }, { $set: {website: website}});
-
-	// TODO: check for waiting users on same webpage
-}
-
-// called on extension close
-function removeEntity(sock_id) {
-	db.collection('active_entities').deleteOne({ sock_id: sock_id });	
-}
-
-// called when a user attempts to open a chat 
-async function getHelper(website) {
-
-	// TODO (REACH): find way to check to make sure this helper hasn't already failed to help a user with a given problem
-	// i.e. don't return same helper to user chat 
-
-	// find an idle helper 
-	var helper_find_promise = db.collection('active_entities').findOne({ website: website, is_chatting: false, entity_type: "Helper"});
-	var helper_data = await helper_find_promise;
-
-	// there are no idle helpers 
-	if (helper_data === null) {
-		console.log("no idle helpers on same website");
-		// TODO: how do we want to handle this case?
-		return null; 
-	}
-
-	var helper_id = helper_data["sock_id"];
-
-	// update the idle helper to be in a chat currently
-	db.collection('active_entities').updateOne({ sock_id: helper_id }, { $set: {is_chatting: true}} );
-
-	return helper_id;
-}
+///////////////////////////////////////////////////////////////////////////////
+////////////////////// Socket.IO functionality  //////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
 
 // Socket connection
 // Creates new socket server for socket
@@ -114,13 +56,25 @@ function init_socket_server() {
 			console.log(data.website)
 			console.log(data.id);
 			socket.join(data.id);
-			addEntity(data.id, data.type, data.website);
+
+			// stores in active_entities mongo collection
+			new_entity_dict = {
+				sock_id: data.id,
+				entity_type: data.type,
+				website: data.website,
+				is_chatting: false,
+				is_waiting: false
+			}
+			storeData("active_entities", new_entity_dict);
+
+			// TODO: check for waiting users on the same webpage if helper is joining
+
 		});
 		// Send init message
 		socket.on('first message', async function(data){
 			if (data.type == "Helper") {//helper
 				console.log(data.website);
-				var helper_id = await getHelper("https://www.yahoo.com/");
+				var helper_id = await getHelper(data.id, "https://www.yahoo.com/");
 				console.log(helper_id); 
 				io.to(helperID).emit('message', {msg: data.msg, callbackID: data.id});
 				io.to(data.id).emit('message', {msg: data.msg, callbackID: helperID});
@@ -132,7 +86,7 @@ function init_socket_server() {
 			if (!data.callbackID){
 				console.log("First Message");
 				console.log(data.website);
-				var helper_id = await getHelper("https://www.yahoo.com/")
+				var helper_id = await getHelper(data.id, "https://www.yahoo.com/")
 				console.log(helper_id);
 				io.to(helper_id).emit('message', {msg: data.msg, callbackID: data.id})					
 			}
@@ -143,18 +97,115 @@ function init_socket_server() {
 	});
 }
 
+///////////////////////////////////////////////////////////////////////////////
+////////////////////// server / routing functions /////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
 // Node application
 
 // Create HTTP server for node application 
-var server = require('http').Server(app);
+function init_http_server() {
+	http_server = require('http').Server(app);
 
-// Node application will be running on 3000 port
-server.listen(serverPort, function(){
-	console.log('HTTP server listening on *:3000');
-});
+	// Node application will be running on 3000 port
+	http_server.listen(serverPort, function(){
+		console.log('HTTP server listening on *:3000');
+	});
 
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html')
-});
+	app.get('/', function(req, res){
+		res.sendFile(__dirname + '/index.html')
+	});
+
+}
 
 // TODO: API endpoint to return an object of form [{category: string, texts: [string]}]
+
+///////////////////////////////////////////////////////////////////////////////
+//////////////////////// mongo query functions ////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+// called when a new entity activates the browser extension by clicking a toolbar option ('User', 'Helper')
+
+function storeData(collection_name, document_dict) {
+	db.collection(collection_name).save(document_dict, (err, result) => {
+			if (err) { return console.log(err);}
+			console.log("successful storeData call on collection: " + collection_name); 
+	});
+}
+
+// called when an entity changes entity_type
+function updateEntityType(sock_id, type) {
+	// change an entity in the mongo collection--either website or type could change
+	db.collection('active_entities').updateOne({ sock_id: sock_id }, { $set: {entity_type: type}});
+
+	// TODO: check for waiting users on same webpage
+}
+
+// called when an entity changes website
+function updateEntityWebsite(sock_id, website) {
+	// change an entity in the mongo collection--either website or type could change
+	db.collection('active_entities').updateOne({ sock_id: sock_id }, { $set: {website: website}});
+
+	// TODO: check for waiting users on same webpage
+}
+
+// called on extension close or user sign out ? 
+function removeEntity(sock_id) {
+	db.collection('active_entities').deleteOne({ sock_id: sock_id });	
+}
+
+
+// to be used whenever a helper becomes available on a webpage 
+// NOTE: must be called within async function and given await keyword
+async function getWaitingUser(website) {
+	var waiting_user_find_promise = db.collection('active_entities').findOne({ website: website, is_waiting: true});
+	var user_data = await waiting_user_find_promise;
+
+	if (user_data === null) {
+		console.log("no waiting users on website: " + website);
+		return null; 
+	}
+
+	var user_sock_id = user_data["sock_id"];
+	db.collection('active_entities').updateOne({ sock_id: user_sock_id }, { $set: {is_waiting: false}} );
+
+	return user_sock_id;
+}
+
+// called when a user attempts to open a chat
+// will either return idle helper sock_id or save the user as waiting in the db (returning null)
+// NOTE: must be called within async function and given await keyword
+async function getHelper(user_sock_id, website) {
+
+	// TODO (REACH): find way to check to make sure this helper hasn't already failed to help a user with a given problem
+	// i.e. don't return same helper to user chat 
+
+	// find an idle helper 
+	var helper_find_promise = db.collection('active_entities').findOne({ website: website, is_chatting: false, entity_type: "Helper"});
+	var helper_data = await helper_find_promise;
+
+	// there are no idle helpers 
+	if (helper_data === null) {
+		console.log("no idle helpers on same website");
+
+		// fix for now - update the db to reflect that this user is waiting 
+		// for a helper and couple with detectWaitingUser, which will 
+		// execute every time a helper becomes available on a webpage
+		db.collection('active_entities').updateOne({ sock_id: user_sock_id }, { $set: {is_waiting: true}});
+		return null; 
+	}
+
+	var helper_id = helper_data["sock_id"];
+
+	// update the idle helper to be in a chat currently
+	db.collection('active_entities').updateOne({ sock_id: helper_id }, { $set: {is_chatting: true}} );
+
+	return helper_id;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////// helpers /////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+// YYYY:MM:DD:HH:MM:SS
+function get_date() { return (new Date()).toJSON().slice(0, 19).replace(/[-T]/g, ':');}
