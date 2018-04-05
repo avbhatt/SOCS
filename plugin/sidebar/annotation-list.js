8dd0dae0c6259f855be6481ee1749518db762404
Christina Liu <cyanliu@umich.edu>
2018-04-05 16:19:27 -0400

    mostly final, no voting functionality yet

--- plugin/sidebar/annotation-list.js
+++ plugin/sidebar/annotation-list.js
@@ -1,17 +1,45 @@
 //var currentUrl = window.location.href;
 var currentUrl = "https://en.wikipedia.org/wiki/Static_web_page";
-// form a GET request to the API, getting all annotations from a given url
-// assume response is an array of dicts or something
 // tempResponse is just placeholder data
+
+
+// notes:
+//    category: one of "image", "video", or "keyboard"
+//
 var tempResponse = [{content: [{text: "Dog sleeping on grass",
                                 ups: 5,
                                 downs: 0}],
                      category: "image",
-                     element: "#pretend-id"}
+                     element: "#pretend-id"},
+                     {content: [{text: "Dog barking at grass for 5 seconds, then playing with a toy",
+                                 ups: 5,
+                                 downs: 0}],
+                      category: "video",
+                      element: "#pretend-id"},
+                      {content: [{text: "Login form",
+                                  ups: 5,
+                                  downs: 0}],
+                       category: "keyboard",
+                       element: "#pretend-id"}
                    ];
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+   coll[i].addEventListener("click", function() {
+       this.classList.toggle("active");
+       var content = this.nextElementSibling;
+       if (content.style.display === "block") {
+           content.style.display = "none";
+       } else {
+           content.style.display = "block";
+       }
+   });
+}
+
 
 // Renders an
-$(function() {
+$(function () {
   // call getWebsitesAnnotations(currentUrl)
   // route: /getAnnotations
 
@@ -19,16 +47,20 @@ $(function() {
   //
   //   tempResponse = data;
   // });
-
   tempResponse.forEach(function(elem) {
-    var annotationItem = $("<li></li>").text(elem.content[0].text);
-
+    var $annotationItem = $("<li></li>").text(elem.category + ": " + elem.content[0].text);
+    $annotationItem.append($("<br aria-hidden='true'>"));
+    $annotationItem.append($("<a>[upvote]</a>"));
+    $annotationItem.append($("<a>[downvote]</a>"));
     if (elem.category == "image") {
-
+      $('#img').css("display", "block")
+      $("#img").next().children('ul').append($annotationItem);
     } else if (elem.category == "video") {
-
+      $('#vid').css("display", "block")
+      $("#vid").next().children('ul').append($annotationItem);
     } else if (elem.category == "keyboard") {
-
+      $('#keyb').css("display", "block")
+      $("#keyb").next().children('ul').append($annotationItem);
     }
 
 
