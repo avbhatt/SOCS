4e36cae35d6b426dcbef005a600dc26d809ce266
Adit Bhatt <avbhatt@umich.edu>
2018-04-05 15:40:08 -0400

    refactor: switch to db lookups instead of local storage

--- plugin/background.js
+++ plugin/background.js
@@ -1,54 +1,60 @@
 var socket = io('http://localhost:3002');
 socket.on('connect', () => {
 	console.log(socket.id)
-	var userThen = browser.storage.local.get("userType");
-	userThen.then((resolve, reject) => {
-		var type = resolve.userType;
-		console.log(type);
-		browser.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
-			var activeTab = arrayOfTabs[0];
-     		var url = activeTab.url; 
-     		console.log(url);
-     		browser.storage.local.set({tab: url});
-			if (type == "Helper"){
-				socket.emit('join', {id: socket.id, website: url, type: "Helper"});
-			}
-			else {
-				browser.storage.local.set({userType: "User"});
-				socket.emit('join', {id: socket.id, website: url, type: "User"});
-			}
-		});
-	});
+	$.get('http://localhost:3000/getEntityInfo', {socket_id: socket.id}, function(data) {
+		if (data) {
+			var website = data.website;
+			var type = data.entity_type;
+			console.log(type);
+			console.log(website)
+			browser.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
+				var activeTab = arrayOfTabs[0];
+				var url = activeTab.url; 
+				console.log(url);
+				if (socket.id && url) {
+					socket.emit('leave', {id: socket.id, website: website, type: type});
+					socket.emit('join', {id: socket.id, website: tab.url, type: type});
+				}
+			});
+		}
+		else {
+			browser.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
+				var activeTab = arrayOfTabs[0];
+				var url = activeTab.url; 
+				console.log(url);
+				if (socket.id && url) {
+					//socket.emit('leave', {id: socket.id, website: tab.url, type: type});
+					socket.emit('join', {id: socket.id, website: tab.url, type: type});
+				}
+			});
+		}	
+	});		
+
 
 	function websiteUpdate(tabid, changeInfo, tab){
 		console.log("ChangeWebsite");
 		console.log("TAB");
 		console.log(tab.url)
-		browser.storage.local.set({tab: tab.url});
-		browser.storage.local.get("userType").then((resolve, reject) => {
-			if (socket.id && tab.url) {
-				console.log("BG 27: " + socket.id);
-				socket.emit('leave', {id: socket.id, website: tab.url, type: "Helper"});
-				var type = resolve.userType;
-				if (type == "Helper"){
-					socket.emit('join', {id: socket.id, website: tab.url, type: "Helper"});
-				}
-				else {
-					browser.storage.local.set({userType: "User"});
-					socket.emit('join', {id: socket.id, website: tab.url, type: "User"});
-				}
+		$.get('http://localhost:3000/getEntityInfo', {socket_id: socket.id}, function(data) {
+			var website = data.website;
+			var type = data.entity_type;
+			console.log(type)
+			console.log(website)
+			if (socket.id && website) {
+				socket.emit('leave', {id: socket.id, website: tab.url, type: type});
+				socket.emit('join', {id: socket.id, website: tab.url, type: type});
 			}
 		});
 	}
 
-	function typeUpdate(changes, areaName){
-		if (areaName == "local"){
-			if (changes.tab) {
+// function typeUpdate(changes, areaName){
+// 	if (areaName == "local"){
+// 		if (changes.tab) {
 
-			}
-		}
-	}
+// 		}
+// 	}
+// }
 
-	browser.storage.onChanged.addListener(typeUpdate);
+// browser.storage.onChanged.addListener(typeUpdate);
 	browser.tabs.onUpdated.addListener(websiteUpdate);
 });
--- plugin/sidebar/chat.js
+++ plugin/sidebar/chat.js
@@ -3,33 +3,31 @@ $(function (){
 	background.then((fulfilled, rejected) => {
 		var socket = fulfilled.socket;
 		// connect to local storage to get type, default to user
-		var userThen = browser.storage.local.get("userType");
-		userThen.then((fulfilled, rejected) => {
-			var user = fulfilled.userType;
-			var recData = {};
-			var websiteThen = browser.storage.local.get("tab");
-			websiteThen.then((fulfilled, rejected) => {
-				var website = fulfilled.tab;
-				$('form').submit(function(){
-					if (recData.callbackID){
-						console.log("CALLBACK")
-						socket.emit('message', {id: socket.id, website: website, type: user, msg: $('#m').val(), callbackID: recData.callbackID})
-					}
-					else {
-						console.log(website)
-						socket.emit('message', {id: socket.id, website: website, type: user, msg: $('#m').val(), callbackID: null});
-					}
-					$('#m').val('');
-					return false;
-				});
-				socket.on('message', function(data){
-					console.log("RECEIVED");
-					console.log(data.msg);
-					$('#messages').append($('<li>').text(data.msg));
-					recData.callbackID = data.callbackID;
-				});
+		var recData = {};
+		$('form').submit(function(){
+			console.log($('#m').val());
+			$.get('http://localhost:3000/getEntityInfo', {socket_id: socket.id}, function(data) {
+				var user = data.entity_type;
+				var website = data.website;
+				console.log(user);
+				console.log(website);
+				if (recData.callbackID) {
+					console.log("CALLBACK")
+					socket.emit('message', {id: socket.id, website: website, type: user, msg: $('#m').val(), callbackID: recData.callbackID})
+				}
+				else {
+					console.log(website)
+					socket.emit('message', {id: socket.id, website: website, type: user, msg: $('#m').val(), callbackID: null});
+				}
+				$('#m').val('');
+				return false;
 			});
+		});	
+		socket.on('message', function(data){
+			console.log("RECEIVED");
+			console.log(data.msg);
+			$('#messages').append($('<li>').text(data.msg));
+			recData.callbackID = data.callbackID;
 		});
 	});
 });
-
