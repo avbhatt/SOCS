505db85fde588889b1d0f8191b8dd3cb943c9032
Adit Bhatt <avbhatt@umich.edu>
2018-04-07 13:06:10 -0400

    refactor/fix/feat: change to messaging paradigm + working chat

--- plugin/sidebar/chat.js
+++ plugin/sidebar/chat.js
@@ -1,33 +1,20 @@
-$(function (){
-	var background = browser.runtime.getBackgroundPage();
-	background.then((fulfilled, rejected) => {
-		var socket = fulfilled.socket;
-		// connect to local storage to get type, default to user
-		var recData = {};
-		$('form').submit(function(){
-			console.log($('#m').val());
-			$.get('http://localhost:3000/getEntityInfo', {socket_id: socket.id}, function(data) {
-				var user = data.entity_type;
-				var website = data.website;
-				console.log(user);
-				console.log(website);
-				if (recData.callbackID) {
-					console.log("CALLBACK")
-					socket.emit('message', {id: socket.id, website: website, type: user, msg: $('#m').val(), callbackID: recData.callbackID})
-				}
-				else {
-					console.log(website)
-					socket.emit('message', {id: socket.id, website: website, type: user, msg: $('#m').val(), callbackID: null});
-				}
-				$('#m').val('');
-				return false;
-			});
-		});	
-		socket.on('message', function(data){
-			console.log("RECEIVED");
-			console.log(data.msg);
-			$('#messages').append($('<li>').text(data.msg));
-			recData.callbackID = data.callbackID;
-		});
+function handleMessage(request, sender, sendResponse){
+	if (request.type == "message_receive") {
+		console.log(request.msg);
+		$('#messages').append($('<li>').text(request.msg));
+	}
+	sendResponse("Received Message");
+}
+
+browser.runtime.onMessage.addListener(handleMessage);
+console.log("SIDEBAR RELOAD");
+$('form').submit(function(){
+	console.log("SUBMIT");
+	var msg = $('#m').val();
+	console.log(msg);
+	browser.runtime.sendMessage({type: "message_send", msg: msg}, function(response) {
+		console.log(response.msg);
 	});
+	$('#m').val('');
+	return false;
 });
