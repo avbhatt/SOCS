4fd1992ddf0716a1f00bc6094e205763924f6695
Adit Bhatt <avbhatt@umich.edu>
2018-04-07 13:17:58 -0400

    fix: remove unnecessary + simpleStore

--- index.js
+++ index.js
@@ -45,8 +45,6 @@ MongoClient.connect('mongodb://server:wah123@ds127044.mlab.com:27044/wah_db', as
 function init_socket_server() {
 	socketServer = require('http').createServer(app);
 	io = require('socket.io')(socketServer);
-	var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
-	var regex = new RegExp(expression);
 
 	// Listen for socket connection on port 3002
 	socketServer.listen(socketPort, function(){
@@ -58,38 +56,23 @@ function init_socket_server() {
 		// Join personalized chat room
 		socket.on('join', function(data){
 			console.log("JOIN START")
-			console.log(data.website)
-			console.log(data.id);
-			console.log(data.type)
 			if (data.id) {
 				socket.join(data.id);
 			}
 			// stores in active_entities mongo collection
-			if (data.website && (data.website).match(regex)) {
-
-        new_entity_dict = { sock_id: data.id, entity_type: data.type, website: data.website, is_chatting: false, is_waiting: false };
-				storeData("active_entities", new_entity_dict);
-			}
+			new_entity_dict = { sock_id: data.id, entity_type: data.type, is_chatting: false, is_waiting: false };
+			//storeData("active_entities", new_entity_dict);
+			storeDataSimple("active_entities", new_entity_dict);
 			console.log("JOIN END")
-
+		});
 			// TODO: check for waiting users on the same webpage if helper is joining
-      // if (data.type == "Helper") {
-      //  var waiting_user_sock_id = await getWaitingUser(data.website);
-      //  if (waiting_user_sock_id !== null) {
-      //    // have the helper send an initialization message, as in below? 
-      //  }
-      // }
-		});
-
-		socket.on('leave', function(data) {
-			console.log("LEAVE START");
-			socket.leave(data.id);
-			console.log(data.id);
-			if (data.website && (data.website).match(regex)) {
-				removeEntity(data.id);
-			}
-			console.log("LEAVE END")
-		});
+		// if (data.type == "Helper") {
+		//  var waiting_user_sock_id = await getWaitingUser(data.website);
+		//  if (waiting_user_sock_id !== null) {
+		//    // have the helper send an initialization message, as in below? 
+		//  }
+		// }
+		
 		
 
 		// Send message
@@ -101,18 +84,18 @@ function init_socket_server() {
 				console.log("passed in website is: " + data.website);
 				var helper_id = await getHelper(data.id, data.website)
 				console.log(helper_id);
-        // store msg in db? 
-        new_msg = { to: helper_id, from: data.id, message: data.msg, time: get_date() };
-        storeData("message_logs", new_msg);
-				io.to(helper_id).emit('message', {msg: data.type + ": " + data.msg, callbackID: data.id});
-				io.to(data.id).emit('message', {msg: data.type + ": " + data.msg, callbackID: helper_id});				
+				// store msg in db? 
+				new_msg = { to: helper_id, from: data.id, message: data.msg, time: get_date() };
+				storeData("message_logs", new_msg);
+				io.to(helper_id).emit('message', {msg: data.msg, callbackID: data.id});
+				io.to(data.id).emit('message', {msg: data.msg, callbackID: helper_id});				
 			}
 			else {
-        // store msg in db? 
-        new_msg = { to: data.callbackID, from: data.id, message: data.msg, time: get_date() };
-        storeData("message_logs", new_msg);
-				io.to(data.callbackID).emit('message', {msg: data.type + ": " + data.msg, callbackID: data.id});
-				io.to(data.id).emit('message', {msg: data.type + ": " + data.msg, callbackID: data.callbackID});
+				// store msg in db? 
+				new_msg = { to: data.callbackID, from: data.id, message: data.msg, time: get_date() };
+				storeData("message_logs", new_msg);
+				io.to(data.callbackID).emit('message', {msg: data.msg, callbackID: data.id});
+				io.to(data.id).emit('message', {msg: data.msg, callbackID: data.callbackID});
 			}
 			console.log("MESSAGE END")
 		});
@@ -120,10 +103,9 @@ function init_socket_server() {
 		// Remove Dead User
 		socket.on('disconnecting', function(data) {
 			console.log("DISCONNECT START");
-			console.log(data.id);
-			if (data.website && (data.website).match(regex)) {
-				removeEntity(data.id);
-			}
+			console.log(socket.id);
+			socket.leave(socket.id);
+			removeEntity(socket.id);
 			console.log("DISCONNECT END");
 		})
 	});
@@ -148,42 +130,55 @@ function init_http_server() {
 		res.sendFile(__dirname + '/index.html')
 	});
 
-  app.get('/getAnnotations', async function(req, res) {
-    console.log("received getEntityInfo request for website: ");
-    console.log(req.query.website);
-    var ann_obj = await getWebsiteAnnotations(req.query.website); 
-    res.send(ann_obj);
-    console.log("serviced getAnnotations request with response: ");
-    console.log(ann_obj);
-  });
+	app.get('/getAnnotations', async function(req, res) {
+	console.log("received getEntityInfo request for website: ");
+	console.log(req.query.website);
+	var ann_obj = await getWebsiteAnnotations(req.query.website); 
+	res.send(ann_obj);
+	console.log("serviced getAnnotations request with response: ");
+	console.log(ann_obj);
+	});
 
-  app.post('/postAnnotation', function(req, res) {
-    console.log("received postAnnotation request with body: ");
-    console.log(req.body);
-    var ann_json = req.body;
-    // not sure if this is necessary--depends on if json object is essentially same as dict
-    var annotation_dict = {website: ann_json["website"], category: ann_json["category"], text: ann_json["text"], upvotes: 0, downvotes: 0};
-    storeData("annotations", annotation_dict);
-    console.log("serviced postAnnotation request");
-  });
-  
-  // give socket_id, get entity_type and website
-  app.get('/getEntityInfo', async function(req, res) {
-    console.log("received getEntityInfo request with socket_id: ");
-    console.log(req.query.socket_id);
-    var entity_info = await getEntityInfo(req.query.socket_id); 
-    res.send(entity_info);
-    console.log("serviced getEntityInfo request with response: ");
-    console.log(entity_info);
-  });
+	app.post('/postAnnotation', function(req, res) {
+	console.log("received postAnnotation request with body: ");
+	console.log(req.body);
+	var ann_json = req.body;
+	// not sure if this is necessary--depends on if json object is essentially same as dict
+	var annotation_dict = {website: ann_json["website"], category: ann_json["category"], text: ann_json["text"], upvotes: 0, downvotes: 0};
+	storeData("annotations", annotation_dict);
+	console.log("serviced postAnnotation request");
+	});
+	
+	// give socket_id, get entity_type and website
+	app.get('/getEntityInfo', async function(req, res) {
+	console.log("received getEntityInfo request with socket_id: ");
+	console.log(req.query.socket_id);
+	var entity_info = await getEntityInfo(req.query.socket_id); 
+	console.log(entity_info);
+	if (entity_info){
+		res.send(entity_info);
+	}
+	else {
+		res.send("Does Not Exist");
+	}
+	
+	console.log("serviced getEntityInfo request with response: ");
+	console.log(entity_info);
+	});
 
+	app.post('/updateEntityType', function(req, res) {
+	console.log("received updateEntityType request with body: ");
+	console.log(req.body);
+	updateEntityType(req.body.socket_id, req.body.entity_type);
+	console.log("serviced updateEntityType request");
+	}); 
 
-  app.post('/updateEntityType', function(req, res) {
-    console.log("received updateEntityType request with body: ");
-    console.log(req.body);
-    updateEntityType(req.body.socket_id, req.body.entity_type);
-    console.log("serviced updateEntityType request");
-  } 
+	app.post('/updateEntityWebsite', function(req, res) {
+	console.log("received updateEntityWebsite request with body: ");
+	console.log(req.body);
+	updateEntityWebsite(req.body.socket_id, req.body.website);
+	console.log("serviced updateEntityWebsite request");
+	});
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -194,7 +189,8 @@ function init_http_server() {
 
 function storeData(collection_name, document_dict) {
 	console.log("STORE START");
-	db.collection('active_entities').findOne(document_dict, (err, result) => {
+	console.log(document_dict.sock_id);
+	db.collection('active_entities').findOne({sock_id: document_dict.sock_id}, (err, result) => {
 		if (err) { return console.log(err); }
 		console.log("FIND START")
 		console.log(result);
@@ -203,7 +199,7 @@ function storeData(collection_name, document_dict) {
 			console.log("SAVE START");
 			db.collection(collection_name).save(document_dict, (err, result) => {
 				if (err) { return console.log(err);}
-				console.log("successful storeData call on collection: " + collection_name); 
+				console.log("successful storeData call on collection: " + collection_name + " with userID: " + result.ops[0].sock_id); 
 				console.log("SAVE END");
 			});
 		}
@@ -212,15 +208,24 @@ function storeData(collection_name, document_dict) {
 	
 }
 
+function storeDataSimple(collection_name, document_dict) {
+	console.log("SIMPLE STORE")
+	db.collection(collection_name).save(document_dict, (err, result) => {
+		if (err) { return console.log(err);}
+		console.log("successful storeData call on collection: " + collection_name + " with userID: " + result.ops[0].sock_id); 
+		console.log("SIMPLE STORE END");
+	});
+}
+
 // used to delete documents matching the query fields in the collection
 // NOTE: if want to delete entire collection, call delete with empty find_dict
 function deleteData(collection_name, query_fields) {
-  db.collection(collection_name).deleteMany(query_fields);  
+	db.collection(collection_name).deleteMany(query_fields);  
 }
 
 // called to update a document in a given collection
 function updateData(collection_name, query_fields, set_fields) {
-  db.collection(collection_name).updateOne(query_fields, { $set: set_fields});
+	db.collection(collection_name).updateOne(query_fields, { $set: set_fields});
 }
 
 // called when an entity changes entity_type
@@ -254,15 +259,15 @@ function removeEntity(sock_id) {
 }
 
 async function getEntityInfo(sock_id) {
-  var waiting_user_find_promise = db.collection('active_entities').findOne({ sock_id: sock_id });
-  var user_data = await waiting_user_find_promise;
-  if (user_data === null) {
-    return null;
-  }
-  var entity_info = {};
-  entity_info["website"] = user_data["website"];
-  entity_info["entity_type"] = user_data["entity_type"];
-  return entity_info;
+	var waiting_user_find_promise = db.collection('active_entities').findOne({ sock_id: sock_id });
+	var user_data = await waiting_user_find_promise;
+	if (user_data === null) {
+	return null;
+	}
+	var entity_info = {};
+	entity_info["website"] = user_data["website"];
+	entity_info["entity_type"] = user_data["entity_type"];
+	return entity_info;
 }
 
 // to be used whenever a helper becomes available on a webpage 
@@ -322,38 +327,38 @@ async function getHelper(user_sock_id, website) {
 //              ups: int, 
 //        downs: int} ] } ] (sorted in descending order of highest overall score (positive - negative)
 async function getWebsiteAnnotations(website) {
-  var anns_find_promise = db.collection("annotations").find({ website: website });
-  var anns_data = await anns_find_promise.toArray();
+	var anns_find_promise = db.collection("annotations").find({ website: website });
+	var anns_data = await anns_find_promise.toArray();
 
-  var return_obj = []
-  var curr_categories = {}
+	var return_obj = []
+	var curr_categories = {}
 
-  anns_data.forEach((ann) => {
-    curr_cat = ann["category"];
-    if (curr_cat in curr_categories) {
-      for (let i = 0; i < return_obj.length; ++i) {
-        if (curr_cat === return_obj[i]["category"]) {
-          return_obj[i]["texts"].push({text: ann["text"], ups: ann["ups"], downs: ann["downs"]});
-          break;
-        }
-      }
-    }
-    else {
-      curr_categories[curr_cat] = "";
-      return_obj.push({category : curr_cat, texts: [{text: ann["text"], ups: ann["ups"], downs: ann["downs"]}]});
-    }
-  })
+	anns_data.forEach((ann) => {
+	curr_cat = ann["category"];
+	if (curr_cat in curr_categories) {
+		for (let i = 0; i < return_obj.length; ++i) {
+		if (curr_cat === return_obj[i]["category"]) {
+			return_obj[i]["texts"].push({text: ann["text"], ups: ann["ups"], downs: ann["downs"]});
+			break;
+		}
+		}
+	}
+	else {
+		curr_categories[curr_cat] = "";
+		return_obj.push({category : curr_cat, texts: [{text: ann["text"], ups: ann["ups"], downs: ann["downs"]}]});
+	}
+	})
 
-  // sort annotations by overall score (positive - negative)
-  return_obj.forEach((list) => {
-    list["texts"].sort((a, b) => {
-      let b_overall = b["ups"] - b["downs"];
-      let a_overall = a["ups"] - a["downs"];
-      return b_overall - a_overall;
-    })
-  });
+	// sort annotations by overall score (positive - negative)
+	return_obj.forEach((list) => {
+	list["texts"].sort((a, b) => {
+		let b_overall = b["ups"] - b["downs"];
+		let a_overall = a["ups"] - a["downs"];
+		return b_overall - a_overall;
+	})
+	});
 
-  return return_obj;
+	return return_obj;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
