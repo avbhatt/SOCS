1b560bc0f7af9dba2bd6c77c93a8453f274da496
Christina Liu <cyanliu@umich.edu>
2018-04-10 12:45:01 -0400

    ??

--- plugin/sidebar/annotation-list.js
+++ plugin/sidebar/annotation-list.js
@@ -43,9 +43,8 @@ function refetchWebsite(tabId, changeInfo, tab){
     $("#img").next().empty();
     $("#keyb").next().empty();
     $("#vid").next().empty();
-    $('#img').css("display", "none");
-    $('#keyb').css("display", "none");
-    $('#vid').css("display", "none");
+
+
     console.log("inside changeinfo");
     if ((tab.url).match(regex)) {
       console.log("inside taburl");
